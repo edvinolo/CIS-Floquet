5aa7cdf6debfa21442baca6185ab9254aecef64f
kamelstekspizza <edvin.olofsson@matfys.lth.se>
2023-12-05 14:57:59 +0100

    Add files via upload

---
+++ cis_Floquet.py
@@ -0,0 +1,341 @@
+from __future__ import division
+import os 
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.linalg as sl
+import scipy.sparse as sp
+import scipy.sparse.linalg as spl
+
+from effective_Hamiltonian import complex_Rabi
+from class_Floquet import Floquet_system
+sys.path.append('/home/edvinolofsson/bin/')
+from plot_utils import format_plot
+
+
+
+
+z_re = np.loadtxt('dipoles_re_He_l_5_350.dat')
+z_im = np.loadtxt('dipoles_im_He_l_5_350.dat')
+H_re = np.loadtxt('mat_re_He_l_5_350.dat')
+H_im = np.loadtxt('mat_im_He_l_5_350.dat')
+
+z = z_re + 1j*z_im
+#z = np.zeros(z_re.shape,dtype = np.complex128)
+H = H_re + 1j*H_im
+
+
+
+def Floquet(omega,E_0,H,z,N_blocks_up,N_blocks_down,**kwargs):
+    
+    energy = kwargs.get('energy',0.0)
+    plot = kwargs.get('plot',False)
+    tol = kwargs.get('tol',2e-1)
+    I_p = kwargs.get('I_p',None)
+
+
+    N_elements = H.shape[0]
+    I_omega = omega*np.eye(N_elements,dtype = np.complex128)
+
+    m = range(-N_blocks_up,N_blocks_down+1)
+    N_blocks = len(m)    
+
+    N_floquet = N_elements*N_blocks
+    
+    print('')
+    print('Diagonalizing floquet Hamiltonian')
+    print(f'omega: {omega}')
+    print(f'E_0: {E_0}')
+    U_p = E_0**2/(4*omega**2)
+    alpha_0 = E_0/omega**2
+    print(f'U_p: {U_p}')
+    print(f'U_p/omega: {U_p/omega}')
+    if I_p:
+        N_photon = (I_p+U_p)/omega
+        print(f'# of photons to ionize: {N_photon}') 
+
+
+    print(f'Quiver radius: {alpha_0}')
+    print(f'N_blocks: {N_blocks}, Abs: {N_blocks_up}, Em: {N_blocks_down}')
+    print('')
+    
+    
+    
+
+    H_fl = sp.lil_matrix((N_floquet,N_floquet),dtype = np.complex128)
+
+    H_test = sp.lil_matrix(H)
+    print(f'# of nonzero in H: {H_test.nnz}, out of {N_elements**2}')
+
+    for i in range(N_blocks):
+        H_fl[i*N_elements:(i+1)*N_elements,i*N_elements:(i+1)*N_elements] = H + m[i]*I_omega
+
+
+        if i !=N_blocks-1:
+            H_fl[i*N_elements:(i+1)*N_elements,(i+1)*N_elements:(i+2)*N_elements] = z*E_0/2 
+            H_fl[(i+1)*N_elements:(i+2)*N_elements,i*N_elements:(i+1)*N_elements] = z*E_0/2 
+
+    H_fl = sp.csr_matrix(H_fl)#,blocksize = (N_elements,N_elements))
+    print(f'# of nonzero in H_fl: {H_fl.nnz}, out of {N_floquet**2}')
+    
+    #H_fl_sys = Floquet_system(H,z,omega,E_0,N_blocks_up,N_blocks_down,shift = -1e-7)
+
+    #ones = np.ones(N_floquet,dtype = np.complex128)
+    #bs = np.zeros(N_floquet,dtype = np.complex128)
+    #bs[N_blocks_up*N_elements+1] = 1.0
+    #diff = H_fl@ones - H_fl_sys.H_linop@ones
+    #print(f'Relative L_2 norm difference: {sl.norm(diff)/sl.norm(ones)}')
+
+    #spilu = spl.spilu(H_fl)
+    #M = spl.LinearOperator((N_floquet,N_floquet),spilu.solve)
+    #res,info = spl.gmres(H_fl,ones,M = M,maxiter = 40)
+    #print(f'GMRES info: {info}')
+
+    #H_fl_sys.block_solve_setup()
+    #res = H_fl_sys.block_solve(ones)
+    #res_2 = spl.spsolve(H_fl,bs)
+    #res = H_fl_sys.solve(ones)
+    #diff = ones-H_fl_sys.H_linop@res
+    #print(f'Relative L_2 norm difference: {sl.norm(diff)/sl.norm(ones)}')
+
+    
+    
+    
+    eigs,vecs = spl.eigs(H_fl,sigma = energy,k=6) 
+    #eigs,vecs = spl.eigs(H_fl_sys.H_invop,k=6,maxiter=10,tol = 1e-1)#,sigma=0.0,OPinv = H_fl_sys.H_invop)
+
+    print('')
+    print('----------------------------------------')    
+    print('Eigenvalues found by Arnoldi iteration: ')
+    print(eigs)
+    print('----------------------------------------')
+    print('')
+
+    #print(vecs[N_blocks*N_elements:N_blocks*N_elements+2,0])
+    #print(vecs[N_blocks*N_elements:N_blocks*N_elements+2,1])
+
+    eigs_H, vecs_H = spl.eigs(H,sigma = -0.1)
+    for k in range(vecs_H.shape[1]):
+        index = np.argmax(np.abs(vecs_H[:,k]))
+        print(vecs_H[index,k])
+        vecs_H[:,k] *=  np.exp(-1j*np.angle(vecs_H[index,k]))
+
+    a = vecs_H[:,0]
+    b = vecs_H[:,2]
+
+    dipole = np.dot(b,np.matmul(z,a))
+    b *= np.exp(-1j*np.angle(dipole))
+
+    print(f'Dipole: {np.dot(b,np.matmul(z,a))}')
+
+    a_N = np.zeros(N_floquet,dtype = np.complex128)
+    a_N[N_blocks_up*N_elements:(N_blocks_up+1)*N_elements] = a.copy()
+
+    b_N_1 = np.zeros(N_floquet,dtype = np.complex128)
+    b_N_1[(N_blocks_up-1)*N_elements:N_blocks_up*N_elements] = b.copy()
+
+    indices = []
+    for k in range(eigs.shape[0]):
+        #if  np.imag(eigs[k])<0  :
+        #    indices.append(k)
+        print(f'Projection on |a,N>: {np.abs(np.dot(a_N,vecs[:,k]))}')
+        print(f'Projection on |b,N-1>: {np.abs(np.dot(b_N_1,vecs[:,k]))}')
+        if (np.abs(np.dot(a_N,vecs[:,k])) >tol or np.abs(np.dot(b_N_1,vecs[:,k])) >tol) and np.imag(eigs[k])<0  :
+            indices.append(k)
+
+    eigs = eigs[indices]
+    print(f'Kept eigenvalues: {eigs}')
+    vecs = vecs[:,indices]
+
+    indices = np.argsort(np.abs(eigs))
+    #eigs = eigs[indices]
+    #vecs = vecs[:,indices]
+
+
+    c_a_A = np.dot(a_N,vecs[:,0])
+    c_a_B = np.dot(a_N,vecs[:,1])
+    c_b_A = np.dot(b_N_1,vecs[:,0])
+    c_b_B = np.dot(b_N_1,vecs[:,1])
+
+    A = np.array([c_a_A,c_b_A])
+    B = np.array([c_a_B,c_b_B])
+
+    S = np.array([[c_a_A,c_a_B],[c_b_A,c_b_B]])
+    S_1 = sl.inv(S)
+    Lambda = np.diag(eigs)
+    #h = np.matmul(S,np.matmul(Lambda,S_1))
+
+    #h = eigs[0]*np.outer(A,A)/np.dot(A,A) + eigs[1]*np.outer(B,B)/np.dot(B,B)
+    #print(h)
+    #print(sl.eigvals(h))
+
+    for k in range(vecs.shape[1]):
+        
+        
+        vecs[:,k] *= 1./np.sqrt(np.dot(vecs[:,k],vecs[:,k]))
+        index = np.argmax(np.abs(vecs[:,k]))
+        vecs[:,k] *=  np.exp(-1j*np.angle(vecs[index,k]))
+
+    #print(np.dot(a_N,vecs[:,0]))
+    #print(np.dot(a_N,vecs[:,1]))
+    #print(np.dot(b_N_1,vecs[:,0]))
+    #print(np.dot(b_N_1,vecs[:,1]))
+
+    
+
+    
+
+
+
+    h_12 = (eigs[0]-eigs[1])*np.dot(a_N,vecs[:,0])*np.dot(b_N_1,vecs[:,0])
+    print(f'h_12: {h_12}')
+    h_12 = (eigs[1]-eigs[0])*np.dot(a_N,vecs[:,1])*np.dot(b_N_1,vecs[:,1])
+    print(f'h_12: {h_12}')
+    
+    h_11 = 0.5*(eigs[0]+ eigs[1]) -0.5*np.sqrt((eigs[0]-eigs[1])**2-4*h_12**2)
+    h_22 = 0.5*(eigs[0]+ eigs[1]) +0.5*np.sqrt((eigs[0]-eigs[1])**2-4*h_12**2)
+    print(f'h_11: {h_11}')
+    print(f'h_22: {h_22}')
+
+    h = np.array([[h_11,h_12],[h_12,h_22]])
+
+    vals = sl.eigvals(h)
+    print(f'lambda_A: {vals[0]}')
+    print(f'lambda_B: {vals[1]}')
+
+
+    if plot:
+        fig,ax = plt.subplots()
+
+        ax.plot(np.abs(a_N))
+        #ax.plot(np.abs(b_N_1))
+        for k in range(vecs.shape[1]):
+            #if np.abs(np.dot(a_N,vecs[:,k])) >tol:
+            #    ax.plot(np.abs(vecs[:,k]))
+            ax.semilogy(np.abs(vecs[:,k]))
+            if k==1:
+                break
+
+        for i in range(N_blocks):
+            ax.vlines(i*N_elements,1e-8,1,colors='k',linestyles='dashed')
+
+        ax.set_ylim(ymin=1e-8)
+
+    
+        format_plot(fig,ax,'State index','Amplitude')
+
+    return eigs,vecs
+
+def E_au_I_wcm2(I):
+    return np.sqrt(I/(3.51*10**(16)))
+
+omega = 0.375
+E_0=0.024
+#E_0 = 0.09245003270420488
+#E_0 = 0
+#E_0= 1.0e-5
+N_blocks_abs = 5
+N_blocks_em = 5
+
+#eigs,vecs = Floquet(omega,E_0,H,z,N_blocks)
+
+#eigs,vecs = Floquet(omega,E_0,H,z,N_blocks_abs,N_blocks_em,plot = False,I_p = 0.5)
+
+#omega = 0.79720797798132026
+omega_vec = np.linspace(0.78,0.8,9)
+#omega_vec = np.linspace(0.372,0.378,9)
+
+fig_re,ax_re = plt.subplots()
+fig_im,ax_im = plt.subplots()
+for i,omega in np.ndenumerate(omega_vec):
+    eigs,vecs = Floquet(omega,E_0,H,z,N_blocks_abs,N_blocks_em)
+
+    re_max = np.argmax(np.real(eigs[:2]))
+    re_min = np.argmin(np.real(eigs[:2]))
+
+    ax_re.plot(omega,np.real(eigs[re_max]), 'k+')
+    ax_re.plot(omega,np.real(eigs[re_min]),'r+')
+    #ax_re.plot(omega,np.real(eigs[2]),'gs')
+    
+
+    ax_im.semilogy(omega,-2*np.imag(eigs[re_max]), 'k+')
+    ax_im.semilogy(omega,-2*np.imag(eigs[re_min]),'r+')
+    #ax_im.semilogy(omega,-2*np.imag(eigs[2]),'gs')
+
+    AT_split = np.abs(np.real(eigs[0]-eigs[1]))
+    print(eigs)
+    print(f'Estimated AT-splitting: {AT_split}')
+
+format_plot(fig_re,ax_re,'Frequency [a.u.]','Re $E$ [a.u.]')
+format_plot(fig_im,ax_im,'Frequency [a.u.]','$\gamma$ [a.u.]')
+
+
+omega = 0.79720797798132026
+#omega = 0.79720797798132026/4.5
+#omega = 0.375
+#omega = 0.5/(1.4*np.pi)
+
+I = np.logspace(np.log10(1.5e12),np.log10(1.5e14),10)
+E_vec = E_au_I_wcm2(I)
+
+fig_re,ax_re = plt.subplots()
+fig_im,ax_im = plt.subplots()
+cmap = plt.get_cmap("tab10")
+
+for i,E_0 in np.ndenumerate(E_vec):
+    eigs,vecs = Floquet(omega,E_0,H,z,N_blocks_abs,N_blocks_em,plot = True,I_p = 0.5,energy = 0.0)
+
+    
+
+    re_max = np.argmax(np.real(eigs[:2]))
+    re_min = np.argmin(np.real(eigs[:2]))
+
+    ax_re.plot(I[i],np.real(eigs[re_max]), 'k+')
+    ax_re.plot(I[i],np.real(eigs[re_min]),'r+')
+
+    for k in range(eigs.shape[0]):
+        ax_re.plot(I[i],np.real(eigs[k]), '+',color = cmap(k+1))
+        ax_im.loglog(I[i],-2*np.imag(eigs[k]), '+',color = cmap(k+1))
+
+    
+
+    ax_im.loglog(I[i],-2*np.imag(eigs[re_max]), 'k+')
+    ax_im.loglog(I[i],-2*np.imag(eigs[re_min]),'r+')
+
+    AT_split = np.abs(np.real(eigs[0]-eigs[1]))
+    print(f'Estimated AT-splitting: {AT_split}')
+
+    atom  = 'He'
+    if atom == 'H':
+        c_R = complex_Rabi(0,0.375,E_0,0.74493553902759979,0.375)
+        c_R.shifts_to_order_4(-4.2994235515557779,11.696868492111513 -1j*0.52234350391660433,
+                                1451.8840693208547 -1j*233.08047840057807,
+                                2887.8029818581272-1j*95.572064969963833,
+                                54.256253682354838+1j*9.3216026855676510,disp=False)
+    elif atom == 'He':
+        c_R = complex_Rabi(0,0.7972081390130366,E_0,0.40401408872998712,
+                             0.7972081390130366)
+    
+        c_R.shifts_to_order_4(-2.8531912039522886,2.0102907828882017 -1j*7.87087926573738425E-003,
+                                155.55015484148478 -1j*10.320460115321691,
+                                120.01707430398346-1j*0.21535244713361390,
+                                8.5330261414000717+1j*0.23255995550376793,disp = True)
+    
+    c_R.compute_eigenvalues(disp=False)
+    print(f'Model h_11: {c_R.h_11}')
+    print(f'Model h_22: {c_R.h_22}')
+    print(f'Model h_12: {c_R.h_12}')
+    print(f'lambda_+: {c_R.lambda_B}')
+    print(f'lambda_-: {c_R.lambda_A}')
+
+    ax_re.plot(I[i],np.real(c_R.lambda_B), 'kx')
+    ax_re.plot(I[i],np.real(c_R.lambda_A),'rx')    
+    
+    ax_im.loglog(I[i],-2*np.imag(c_R.lambda_B), 'kx')
+    ax_im.loglog(I[i],-2*np.imag(c_R.lambda_A),'rx')
+
+format_plot(fig_re,ax_re,'Intensity [W/cm$^2$]','Re $E$ [a.u.]')
+format_plot(fig_im,ax_im,'Intensity [W/cm$^2$]','$\gamma$ [a.u.]')
+
+plt.show()
+
---
+++ class_Floquet.py
@@ -0,0 +1,434 @@
+import numpy as np
+
+#Need to set environment varialbe to use propack
+import os
+os.environ["SCIPY_USE_PROPACK"] = "true"
+import scipy.linalg as sl
+import scipy.sparse as sp
+import scipy.sparse.linalg as spl
+
+
+class Floquet_system:
+    def __init__(self,H,Z,omega,E_0,N_blocks_abs,N_blocks_em,shift = 0.0):
+        self.H = H.copy()
+        self.V = 0.5*E_0*Z.copy()
+        self.omega = omega
+        self.E_0 = E_0
+        self.N_blocks_abs = N_blocks_abs
+        self.N_blocks_em = N_blocks_em
+        self.shift = shift
+
+        self.N_elements = self.H.shape[0]
+
+        self.N_floquet_blocks = self.N_blocks_abs + self.N_blocks_em +1
+        self.N_floquet = self.N_elements*self.N_floquet_blocks
+
+        self.m_omega = np.array([m*self.omega for m in range(-self.N_blocks_abs,self.N_blocks_em+1)])
+        print(self.omega)
+        print(self.m_omega)
+
+        
+
+        self.diag = np.zeros(self.N_floquet,dtype = np.complex128)
+        self.diag_inv = np.zeros(self.N_floquet,dtype = np.complex128)
+        for i in range(self.N_floquet_blocks):
+            self.diag[i*self.N_elements:(i+1)*self.N_elements] = np.diag(self.H) + self.m_omega[i]
+
+        for i in range(self.N_floquet):
+            if np.abs(self.diag[i]) >= 1e-13:
+                self.diag_inv[i] = 1.0/self.diag[i]
+
+        print(self.diag_inv)
+        
+        self.M_inv = sp.diags(self.diag_inv)
+        self.M = sp.diags(self.diag)
+
+        self.H = sp.csc_array(self.H)
+        self.V = sp.csc_array(self.V)
+
+
+        self.H_linop = spl.LinearOperator((self.N_floquet,self.N_floquet),matvec = self.matvec,rmatvec=self.rmatvec)
+        eigs = spl.eigs(self.H_linop,k=6,return_eigenvectors=False)
+        self.weight = 1.0/eigs[np.argmax(np.abs(eigs))]
+        self.H_0_linop = spl.LinearOperator((self.N_floquet,self.N_floquet),matvec = self.H_0_matvec,rmatvec=self.H_0_rmatvec)
+        self.H_invop = spl.LinearOperator((self.N_floquet,self.N_floquet),matvec = self.solve)
+
+        
+
+
+
+        return
+
+    def matvec(self,v):
+        result = np.zeros(self.N_floquet,dtype = np.complex128)
+
+        for i in range(self.N_floquet_blocks):
+            result[i*self.N_elements:(i+1)*self.N_elements] += self.H@v[i*self.N_elements:(i+1)*self.N_elements] + (self.m_omega[i])*v[i*self.N_elements:(i+1)*self.N_elements]
+            
+            if i != self.N_floquet_blocks-1:
+                result[i*self.N_elements:(i+1)*self.N_elements] += self.V@v[(i+1)*self.N_elements:(i+2)*self.N_elements]
+                result[(i+1)*self.N_elements:(i+2)*self.N_elements] += self.V@v[i*self.N_elements:(i+1)*self.N_elements]
+
+        return result
+    
+    def matvec_weight(self,v):
+        return self.weight*self.matvec(v)
+    
+    def matvec_non_diag(self,v):
+        result = np.zeros(self.N_floquet,dtype = np.complex128)
+
+        for i in range(self.N_floquet_blocks):
+            if i != self.N_floquet_blocks-1:
+                result[i*self.N_elements:(i+1)*self.N_elements] += self.V@v[(i+1)*self.N_elements:(i+2)*self.N_elements]
+                result[(i+1)*self.N_elements:(i+2)*self.N_elements] += self.V@v[i*self.N_elements:(i+1)*self.N_elements]
+
+        return result
+    
+    def matvec_D_E(self,v):
+        result = np.zeros(self.N_floquet,dtype = np.complex128)
+        
+        for i in range(self.N_floquet_blocks):
+            result[i*self.N_elements:(i+1)*self.N_elements] += self.H@v[i*self.N_elements:(i+1)*self.N_elements] + self.m_omega[i]*v[i*self.N_elements:(i+1)*self.N_elements]
+            if i != self.N_floquet_blocks-1:
+                result[(i+1)*self.N_elements:(i+2)*self.N_elements] += self.V@v[i*self.N_elements:(i+1)*self.N_elements]
+
+        return result
+    
+    def matvec_F(self,v):
+        result = np.zeros(self.N_floquet,dtype = np.complex128)
+
+        for i in range(self.N_floquet_blocks):
+            if i != self.N_floquet_blocks-1:
+                result[i*self.N_elements:(i+1)*self.N_elements] += self.V@v[(i+1)*self.N_elements:(i+2)*self.N_elements]
+        return result
+    
+    def H_0_matvec(self,v):
+        result = np.zeros(self.N_floquet,dtype = np.complex128)
+
+        for i in range(self.N_floquet_blocks):
+            result[i*self.N_elements:(i+1)*self.N_elements] += self.H@v[i*self.N_elements:(i+1)*self.N_elements] + (self.m_omega[i]+1e2)*v[i*self.N_elements:(i+1)*self.N_elements]
+            
+        return result
+    
+    def rmatvec(self,v):
+        result = np.zeros(self.N_floquet,dtype = np.complex128)
+
+        for i in range(self.N_floquet_blocks):
+            result[i*self.N_elements:(i+1)*self.N_elements] += np.conjugate(self.H)@v[i*self.N_elements:(i+1)*self.N_elements] + self.m_omega[i]*v[i*self.N_elements:(i+1)*self.N_elements]
+            
+            if i != self.N_floquet_blocks-1:
+                result[i*self.N_elements:(i+1)*self.N_elements] += np.conjugate(self.V)@v[(i+1)*self.N_elements:(i+2)*self.N_elements]
+                result[(i+1)*self.N_elements:(i+2)*self.N_elements] += np.conjugate(self.V)@v[i*self.N_elements:(i+1)*self.N_elements]
+
+        return result
+    
+    def H_0_rmatvec(self,v):
+        result = np.zeros(self.N_floquet,dtype = np.complex128)
+
+        for i in range(self.N_floquet_blocks):
+            result[i*self.N_elements:(i+1)*self.N_elements] += np.conjugate(self.H)@v[i*self.N_elements:(i+1)*self.N_elements] + self.m_omega[i]*v[i*self.N_elements:(i+1)*self.N_elements]
+            
+        return result
+    
+    def H_matvec_Neu(self,v,weight = 1.0):
+        b = weight*self.matvec(v)
+
+        return v - b
+    
+    def neumann(self,v,num_iter = 10):
+        result = np.zeros(self.N_floquet,dtype = np.complex128)
+
+        eigs = spl.eigs(self.H_linop,k=6,return_eigenvectors=False)
+        print(eigs)
+        weight = 1.0/eigs[np.argmax(np.abs(eigs))]
+
+        temp = v.copy()
+        for i in range(num_iter):
+            temp = self.H_matvec_Neu(temp,weight)
+            result += temp
+
+        diff = v-self.H_linop@(v+result)
+
+        if sl.norm(v)>0:
+            print(f'Relative forward error: {sl.norm(diff)/sl.norm(v)}, num_iter: {i+1}')
+
+        return v + result
+
+
+    def svd(self):
+        print('')
+        print('Doing svd ...')
+        self.u,self.s,self.vh = spl.svds(self.H_linop,which = 'SM',solver = 'propack',maxiter=8)
+        self.n_sing_vals = self.s.shape[0]
+        print('svd done!')
+        print('')
+
+        return
+    
+    def svd_precond(self,b):
+        result = np.zeros(self.N_floquet,dtype=np.complex128)
+        for i in range(self.n_sing_vals):
+            result += 1.0/self.s[i]*np.dot(self.u[:,i],b)*self.vh[i,:] 
+
+        return result
+    
+    def jacobi(self,b,max_iter = 200,rtol = 1e-5):
+
+        result = np.zeros(self.N_floquet,dtype = np.complex128)
+
+        eigs = spl.eigs(self.H_linop,k=6,return_eigenvectors=False)
+        #print(eigs)
+        weight = 1.0/eigs[np.argmax(np.abs(eigs))]
+
+        RHS_nonzero = True
+        if sl.norm(b) == 0.0:
+            RHS_nonzero = False
+
+        for i in range(max_iter):
+            #result = self.M_inv@(b-self.H_linop@result+self.M@result)
+            result = result + weight*self.M_inv@(b-self.H_linop@result)
+            diff = self.H_linop@result-b
+
+            if RHS_nonzero:
+                if sl.norm(diff)/sl.norm(b) < rtol:
+                    break
+            elif sl.norm(diff) < rtol:
+                break 
+        
+        if RHS_nonzero:
+            print(f'Relative forward error: {sl.norm(diff)/sl.norm(b)}, num_iter: {i+1}')
+
+        return result
+    
+    def jacobi_block(self,b,max_iter=5,rtol=1e-5):
+        result = np.zeros(self.N_floquet,dtype = np.complex128)
+
+        RHS_nonzero = True
+        if sl.norm(b) == 0.0:
+            RHS_nonzero = False
+
+        for i in range(max_iter):
+            result = self.jacobi(b) - self.jacobi(self.matvec_non_diag(result))
+            diff = self.H_linop@result-b
+
+            if RHS_nonzero:
+                if sl.norm(diff)/sl.norm(b) < rtol:
+                    break
+            elif sl.norm(diff) < rtol:
+                break 
+
+        if RHS_nonzero:
+            print(f'Relative forward error: {sl.norm(diff)/sl.norm(b)}, num_iter: {i+1}')
+
+        return result
+    
+    def gauss_seidel(self,b,max_iter=5,rtol=1e-5):
+        x_k = np.zeros(self.N_floquet,dtype=np.complex128)
+        x_k_1 = np.zeros(self.N_floquet,dtype=np.complex128)
+
+        RHS_nonzero = True
+        if sl.norm(b) == 0.0:
+            RHS_nonzero = False
+
+        for i in range(max_iter):
+            #x_k = self.H_linop@x_k
+
+            x_k = self.matvec_F(x_k)
+            x_k_1  = self.block_gs_solve(b-x_k)
+
+
+            #x_k_1[0] = self.diag_inv[0]*(b[0]-np.sum(x_k[1:]))
+            #for j in range(1,self.N_floquet):
+            #    A_x_k_1 = self.H_linop@x_k_1
+            #    x_k_1[j] = self.diag_inv[j]*(b[j]-np.sum(x_k[j+1:])-np.sum(A_x_k_1[0:j]))
+            
+            
+            
+            x_k = x_k_1.copy()
+            diff = self.H_linop@x_k_1-b
+
+            if RHS_nonzero:
+                if sl.norm(diff)/sl.norm(b) < rtol:
+                    break
+            elif sl.norm(diff) < rtol:
+                break 
+        
+        if RHS_nonzero:
+            print(f'Relative forward error: {sl.norm(diff)/sl.norm(b)}, num_iter: {i+1}')
+
+        return x_k_1
+    
+
+
+    def SOR(self,b,max_iter=2,rtol=1e-5,weight = 1.5e0):
+        x_k = np.zeros(self.N_floquet,dtype=np.complex128)
+        x_k_1 = np.zeros(self.N_floquet,dtype=np.complex128)
+
+        RHS_nonzero = True
+        if sl.norm(b) == 0.0:
+            RHS_nonzero = False
+
+        for i in range(max_iter):
+            #x_k = self.H_linop@x_k
+
+            x_k = -self.weight*self.matvec_F(x_k) + (1-self.weight)*self.H_0_matvec(x_k) 
+            x_k_1  = self.block_SOR_solve(self.weight*b+x_k,self.weight)
+
+
+            #x_k_1[0] = self.diag_inv[0]*(b[0]-np.sum(x_k[1:]))
+            #for j in range(1,self.N_floquet):
+            #    A_x_k_1 = self.H_linop@x_k_1
+            #    x_k_1[j] = self.diag_inv[j]*(b[j]-np.sum(x_k[j+1:])-np.sum(A_x_k_1[0:j]))
+            
+            
+            
+            x_k = x_k_1.copy()
+            diff = self.H_linop@x_k_1-b
+
+            if RHS_nonzero:
+                if sl.norm(diff)/sl.norm(b) < rtol:
+                    break
+            elif sl.norm(diff) < rtol:
+                break 
+        
+        if RHS_nonzero:
+            print(f'Relative forward error: {sl.norm(diff)/sl.norm(b)}, num_iter: {i+1}')
+
+        return x_k_1
+    
+    def gmres_precond(self,b,max_iter=5,rtol=1e-5):
+        print(sl.norm(b))
+        result,info = spl.gmres(self.H_linop,b,maxiter=max_iter,tol=rtol)
+
+        return result
+
+    
+    def bicg(self,b,max_iter=10,rtol=1e-5):
+        RHS_nonzero = True
+        if sl.norm(b) == 0.0:
+            RHS_nonzero = False
+
+        result,info = spl.bicg(self.H_0_linop,b,tol=rtol,maxiter=max_iter)
+        diff = self.H_0_linop@result-b
+        
+        if RHS_nonzero:
+            print(f'Relative forward error: {sl.norm(diff)/sl.norm(b)}, num_iter: {info}')
+
+        return result
+    
+    def lgmres(self,A,b):
+        result,info =spl.lgmres(A,b,tol=1e-13,maxiter=300)
+        if info != 0:
+                    RuntimeError(f'LGMRES exited with: {info}')
+
+        return result
+    
+    def solve(self,b,M=None):
+        sigma = 0.0
+        print('solving')
+        if M is None:
+            #jacobi_linop = spl.LinearOperator((self.N_floquet,self.N_floquet),matvec = self.jacobi)
+            #gs_linop = spl.LinearOperator((self.N_floquet,self.N_floquet),matvec = self.gauss_seidel)
+            SOR_linop = spl.LinearOperator((self.N_floquet,self.N_floquet),matvec = self.SOR)
+            #bicg_linop = spl.LinearOperator((self.N_floquet,self.N_floquet),matvec = self.bicg)
+            #neumann_linop = spl.LinearOperator((self.N_floquet,self.N_floquet),matvec = self.neumann)
+            #gmres_linop = spl.LinearOperator((self.N_floquet,self.N_floquet),matvec = self.gmres_precond)
+            #weight_linop = spl.LinearOperator((self.N_floquet,self.N_floquet),matvec = self.matvec_weight)
+            #self.svd()
+            #svd_linop = spl.LinearOperator((self.N_floquet,self.N_floquet),matvec = self.svd_precond)
+            result,info = spl.gmres(self.H_linop,b,M=SOR_linop,maxiter=100,tol=1e-10,restart = 50)
+            #result,info = spl.lgmres(self.H_linop,b,maxiter=40,tol=1e-10)
+            #result,info = spl.bicgstab(self.H_linop,b,tol=1e-10,maxiter=100)
+
+            #result,info = spl.gmres(self.H_linop,b,maxiter=300,tol=1e-10,restart=50)
+            #result /=self.weight  
+
+            #result = jacobi_linop@b
+        else:
+            result,info = spl.gmres(self.H_linop,b,M=M,maxiter=40)
+        print(f'GMRES info: {info}')
+        print(f'{sl.norm(self.H_linop@result-b)}')
+        
+
+        return result
+    
+    def block_solve_setup(self):
+        print('')
+        print('Doing LU factorization of block matrix...')
+        self.Blocks = self.N_floquet_blocks*[sp.csc_array((self.N_elements,self.N_elements),dtype = np.complex128)]
+        self.factors = []
+        print(len(self.Blocks))
+
+        identity = sp.identity(self.N_elements,dtype =np.complex128, format='csc')
+
+        self.Blocks[0] = self.H + self.m_omega[0]*identity
+        self.factors.append(spl.splu(self.Blocks[0]))
+        for i in range(self.N_floquet_blocks-1):
+            for j in range(self.N_elements):
+                #w = self.lgmres(self.Blocks[i],self.Z[:,j])
+                w = self.factors[i].solve(self.V[:,[j]].toarray())
+                self.Blocks[i+1][:,[j]] = self.H.getcol(j)-self.V@w
+            self.Blocks[i+1] += self.m_omega[i+1]*identity
+            self.factors.append(spl.splu(self.Blocks[i+1]))
+
+        #self.Blocks = [sp.csr_array(Block) for Block in self.Blocks]
+
+        #print(f'# of nonzero in Blocks: {self.Blocks.nnz}, out of {self.N_elements**2*self.N_blocks}')
+        print('Done with LU factorization!')
+        print('')
+
+        return
+    
+    #def check_matrix(self):
+
+    
+    def block_solve(self,b):
+        
+        print('')
+        print('Solving with factorized block matrix...')
+        
+        #y = sp.lil_array((self.N_elements,self.N_blocks),dtype = np.complex128)
+        y = np.zeros((self.N_elements,self.N_floquet_blocks),dtype = np.complex128)
+        #y[:,0] = self.lgmres(self.Blocks[0],b[:self.N_elements])
+        y[:,0] = self.factors[0].solve(b[:self.N_elements])
+
+        for i in range(1,self.N_floquet_blocks):
+            #y[:,i] = self.lgmres(self.Blocks[i],b[i*self.N_elements:(i+1)*self.N_elements]-self.Z@b[(i-1)*self.N_elements:i*self.N_elements])
+            y[:,i] = self.factors[i].solve(b[i*self.N_elements:(i+1)*self.N_elements]-self.V@y[:,i-1])
+
+        result = np.zeros(self.N_floquet,dtype=np.complex128)
+
+        result[(self.N_floquet_blocks-1)*self.N_elements:] = y[:,self.N_floquet_blocks-1]
+
+        for i in reversed(range(self.N_floquet_blocks-1)):
+            #w = self.lgmres(self.Blocks[i],self.Z@result[(i+1)*self.N_elements:(i+2)*self.N_elements])
+            w = self.factors[i].solve(self.V@result[(i+1)*self.N_elements:(i+2)*self.N_elements])
+            result[i*self.N_elements:(i+1)*self.N_elements] = y[:,i]-w 
+
+        print('Done!')
+        print('')
+
+        return result
+
+    def block_gs_solve(self,b):
+        result = np.zeros(b.shape,dtype = np.complex128)
+        id = sp.identity(self.N_elements)
+
+        result[:self.N_elements] = spl.spsolve(self.H+self.m_omega[0]*id,b[:self.N_elements])
+        for i in range(1,self.N_floquet_blocks):
+            result[i*self.N_elements:(i+1)*self.N_elements] = self.lgmres(self.H+self.m_omega[i]*id,b[i*self.N_elements:(i+1)*self.N_elements]-self.V@result[(i-1)*self.N_elements:i*self.N_elements])
+
+        return result
+    
+
+    def block_SOR_solve(self,b,weight):
+        result = np.zeros(b.shape,dtype = np.complex128)
+        id = sp.identity(self.N_elements)
+
+        result[:self.N_elements] = spl.spsolve(self.H+(self.m_omega[0])*id,b[:self.N_elements])
+        for i in range(1,self.N_floquet_blocks):
+            result[i*self.N_elements:(i+1)*self.N_elements] = self.lgmres(self.H+(self.m_omega[i])*id,b[i*self.N_elements:(i+1)*self.N_elements]-weight*self.V@result[(i-1)*self.N_elements:i*self.N_elements])
+
+        return result
+
+    
